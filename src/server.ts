33dc379e661706c190e51fe7ddee3404552acad3
herlanderbento <herlanderbento19@gmail.com>
2024-03-11 13:57:56 +0000

    add values in env.xample

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ const server = http.createServer(app);
 const PORT: number = Number(process.env.MIRANTES_PORT) || 3000;
 
 server.listen(PORT, () => {
-  console.log(` Server is running at http://localhost:${PORT} `);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
